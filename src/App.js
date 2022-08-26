f2d42b103bee8bca2c929869a53442a1c9317dd8
Alexander Laudino <93599746+alexander-laudino@users.noreply.github.com>
2022-08-26 12:41:17 -0400

    Add constructor, handleChange and handleSubmit methods, refactor render method

--- src/App.js
+++ src/App.js
@@ -1,5 +1,45 @@
-function App() {
-  return <div className="App"></div>;
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: "Enter new task:",
+      tasks: [],
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    console.log("A task was added: " + this.state.value);
+    this.setState((prevState) => ({
+      tasks: [...prevState.tasks, this.state.value],
+    }));
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.props.title}</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Task:
+            <textarea value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <p>{this.state.tasks}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
